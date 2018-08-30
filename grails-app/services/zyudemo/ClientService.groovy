73713bd59489fd6648a2a3f2eef6223fb88b09cd
fisher <yb48904@ly.com>
2018-08-30 18:24:20 +0800

    mork微信通过code获取openId的结果

--- grails-app/services/zyudemo/ClientService.groovy
+++ grails-app/services/zyudemo/ClientService.groovy
@@ -1,7 +1,9 @@
 package zyudemo
 
+import grails.converters.JSON
 import grails.gorm.transactions.Transactional
 import groovyx.net.http.HttpBuilder
+import org.grails.web.json.JSONObject
 
 @Transactional
 class ClientService {
@@ -9,16 +11,13 @@ class ClientService {
     def http = HttpBuilder.configure {
         request.uri = "https://api.weixin.qq.com/"
     }
-
-    def http1 = HttpBuilder.configure {
-        request.uri = "api.weixin.qq.com/"
-    }
     def getUserInfoFromWeChat(String code) {
-
+        String demo =  '{"session_key":"KOYQLZ7GUxxXyq7JFIMnVg==","openid":"oKGXT5OWsrtqmBZfmqz72SyuuhgE"}'
         String result = http.get {
-            request.uri.path = URLEncoder.encode("/sns/jscode2session?appid=wx6f46232fa9e41bb3&secret=9ccc8bf3146d4347c1a929c550789298&js_code=${code}&grant_type=authorization_code")
-
+            request.uri.path = "/sns/jscode2session"
+            request.uri.query = [appid: 'wx6f46232fa9e41bb3', secret: '9ccc8bf3146d4347c1a929c550789298', js_code: code, grant_type:'authorization_code']
         }
-        return result
+        def jsonResult = JSON.parse(demo)
+        return jsonResult
     }
 }
